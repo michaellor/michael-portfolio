c16cb26e120c020b746a4e421e7066af8036e275
User <User@Users-MacBook-Pro.local>
2016-05-17 23:57:20 -0700

    add rokkitt font and omit page tpl header content

--- sites/all/themes/myzen/templates/page.tpl.php
+++ sites/all/themes/myzen/templates/page.tpl.php
@@ -20,15 +20,7 @@
 
     <?php if ($site_name || $site_slogan): ?>
       <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
       </div>
     <?php endif; ?>
 
@@ -59,9 +51,9 @@
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
+      <!-- <?php if ($title): ?>
         <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
+      <?php endif; ?> -->
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
