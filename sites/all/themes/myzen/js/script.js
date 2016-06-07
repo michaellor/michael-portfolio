e9a81f1f47fcb4b473f8f465b8b996d2106db739
User <User@Users-MacBook-Pro-2.local>
2016-06-07 16:31:29 -0700

    update css and page tpl

--- sites/all/themes/myzen/js/script.js
+++ sites/all/themes/myzen/js/script.js
@@ -1,24 +1,26 @@
 (function ($) {
   $(document).ready(function(){
 
-    $('.scrollnav').click(function(event) {
-      event.preventDefault();
-      var target = $(this.hash);
-        $('thml, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-    });
+    // $('.scrollnav').click(function(event) {
+    //   event.preventDefault();
+    //   var target = $(this.hash);
+    //     $('thml, body').animate({
+    //       scrollTop: target.offset().top
+    //     }, 1000);
+    // });
 
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-
-      if(scroll >= 100) {
-        $(document.body).addClass('bgchange');
-      }
-      if(scroll <= 100) {
-        $(document.body).removeClass();
-      }
-    });
+    // --- ADD/REMOVE classes for scroll positions ----
+    // $(window).scroll(function() {
+    //   var scroll = $(window).scrollTop();
+    //
+    //   if(scroll <= 100) {
+    //     $(document.body).removeClass();
+    //     $(document.body).addClass('bgorigin');
+    //   }
+    //   if(scroll >= 100) {
+    //     $(document.body).addClass('bgchange');
+    //   }
+    // });
 
 
   });
--- sites/all/themes/myzen/templates/page.tpl.php
+++ sites/all/themes/myzen/templates/page.tpl.php
@@ -10,19 +10,71 @@
 
 <?php print render ($page['hero']); ?>
 
-<div id="page">
+<header class="header" id="header" role="banner">
 
-  <header class="header" id="header" role="banner">
+  <?php if ($secondary_menu): ?>
+    <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+      <?php print theme('links__system_secondary_menu', array(
+        'links' => $secondary_menu,
+        'attributes' => array(
+          'class' => array('links', 'inline', 'clearfix'),
+        ),
+        'heading' => array(
+          'text' => $secondary_menu_heading,
+          'level' => 'h2',
+          'class' => array('element-invisible'),
+        ),
+      )); ?>
+    </nav>
+  <?php endif; ?>
 
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
+  <?php print render($page['header']); ?>
+
+</header>
+
+<div id="name-header">
+  <h2>Michael Lor</h2>
+  <p>Developer + Designer</p>
+  <img src="/sites/all/images/logo-M.svg" id="hero_logo">
+  <p><a href="#project_jump" class="scrollnav">Projects</a></p>
+</div>
+
+<div id="main">
+  <div id="content" class="column" role="main">
+    <?php print render($page['highlighted']); ?>
+    <?php print $breadcrumb; ?>
+    <a id="main-content"></a>
+    <?php print render($title_prefix); ?>
+    <!-- <?php if ($title): ?>
+    <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+  <?php endif; ?> -->
+  <?php print render($title_suffix); ?>
+  <?php print $messages; ?>
+  <?php print render($tabs); ?>
+  <?php print render($page['help']); ?>
+  <?php if ($action_links): ?>
+    <ul class="action-links"><?php print render($action_links); ?></ul>
+  <?php endif; ?>
+  <?php print render($page['content']); ?>
+  <?php print $feed_icons; ?>
+</div>
+
+  <div id="navigation">
+
+    <?php if ($main_menu): ?>
+      <nav id="main-menu" role="navigation" tabindex="-1">
+        <?php
+        // This code snippet is hard to modify. We recommend turning off the
+        // "Main menu" on your sub-theme's settings form, deleting this PHP
+        // code block, and, instead, using the "Menu block" module.
+        // @see https://drupal.org/project/menu_block
+        print theme('links__system_main_menu', array(
+          'links' => $main_menu,
           'attributes' => array(
             'class' => array('links', 'inline', 'clearfix'),
           ),
           'heading' => array(
-            'text' => $secondary_menu_heading,
+            'text' => t('Main menu'),
             'level' => 'h2',
             'class' => array('element-invisible'),
           ),
@@ -30,82 +82,10 @@
       </nav>
     <?php endif; ?>
 
-    <?php print render($page['header']); ?>
-
-  </header>
-
-  <div id="name-header">
-      <h2>Michael Lor</h2>
-      <p>Developer + Designer</p>
-      <p><a href="#project_jump" class="scrollnav">Projects</a></p>
+    <?php print render($page['navigation']); ?>
   </div>
-
-  <div id="main">
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <!-- <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?> -->
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div>
-
-    <div id="navigation">
-
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div>
-
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
-
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside>
-    <?php endif; ?>
-
-  </div>
-
-  <?php print render($page['footer']); ?>
-
 </div>
-
-<div id="project_jump"></div>
-
+<?php print render($page['footer']); ?>
+<div id="page">
+</div>
 <?php print render($page['bottom']); ?>
